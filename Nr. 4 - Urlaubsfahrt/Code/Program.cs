5754b5e45a12c46159aa485d8a6ba5d69582dae7
doge815 <815.doge@gmail.com>
2019-09-05 11:06:29 +0200

    first

---
+++ Nr. 4 - Urlaubsfahrt/Code/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace p
+{
+    public class program
+    {
+        int FuelSize;
+        float Usage;
+        int StartFuel;
+        float Trip;
+        List<GasStation> AllStations;
+
+        List<Track> TrackParts;
+
+        void Main()
+        {
+            Console.WriteLine("Insert path:");
+            string path = Console.ReadLine();
+            string[] FileValues = File.ReadAllText(path).Split('\n');
+            AllStations = new List<GasStation>();
+            for(int i = 5; i < FileValues.Length; i++)
+            {
+                float[] values = FileValues[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmtys).Select(x => (float)x);
+                AllStations.Add(new GasStation(values[0], values[1]));
+            }
+            for(int i = 0; i < AllStations.Count; i ++)
+            {
+                
+            }
+        }
+    }
+
+    public class GasStation
+    {
+        public GasStation(float pricePerVolumeInEuroPerLiter, float position)
+        {
+            PricePerVolumeInEuroPerLiter = pricePerVolumeInEuroPerLiter;
+            Position = position;
+        }
+
+        public float PricePerVolumeInEuroPerLiter {get;}
+        public float Position {get;}
+    }
+
+    public class Track
+    {
+        List<GasStation> Stops {get;}
+        float GetPrice()
+        {
+            float value = 0;
+            for(int i = 0; i < Stops.Count - 1; i++)
+            {
+                value += (Stops[i+1].Position - Stops[i].Position) * Stops[i].PricePerVolumeInEuroPerLiter;
+            }
+            return value;
+        }
+    }
+
+}
