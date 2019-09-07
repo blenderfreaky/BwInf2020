406b0432264025acc49a13076592be84c0e2075b
Doge815 <42283470+Doge815@users.noreply.github.com>
2019-09-07 18:03:28 +0200

    created extension, constructer and a bit of usable code

--- Nr. 4 - Urlaubsfahrt/Code/Program.cs
+++ Nr. 4 - Urlaubsfahrt/Code/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace p
 {
@@ -18,6 +19,7 @@ namespace p
 
         void Main()
         {
+            int FuelLength = 10; //Will be removed later, but its faster to use it right now
             Console.WriteLine("Insert path:");
             string path = Console.ReadLine();
             string[] FileValues = File.ReadAllText(path).Split('\n');
@@ -29,7 +31,13 @@ namespace p
             }
             for(int i = 0; i < AllStations.Count; i ++)
             {
-                
+                List<Track> PosssibleParts = TrackParts.Where(x => AllStations[i].Position - x.Stops.Last().Position > FuelLength);
+                if(PosssibleParts.Count == 0 && FuelLength < AllStations[i].Position)
+                {
+                    throw new Exception();
+                }
+                Track t = PosssibleParts.M(x => x.Stops.Count);
+                TrackParts.Add(new Track(PosssibleParts.AllMins(x => x.Stops.Count).Min(x => x.GetPrice())));
             }
         }
     }
@@ -48,16 +56,50 @@ namespace p
 
     public class Track
     {
-        List<GasStation> Stops {get;}
-        float GetPrice()
+        public List<GasStation> Stops {get;}
+        float GetPrice(int pos)
         {
             float value = 0;
             for(int i = 0; i < Stops.Count - 1; i++)
             {
                 value += (Stops[i+1].Position - Stops[i].Position) * Stops[i].PricePerVolumeInEuroPerLiter;
             }
+            value += (pos - Stops.Last().Position) * Stops.Last().PricePerVolumeInEuroPerLiter;
             return value;
+        }
+        public Track(GasStation s)
+        {
+            Stops = new List<GasStation> { s };
+        }
+        public Track (Track t, GasStation s)
+        {
+            Stops = new List<GasStation>();
+            foreach (GasStation ss in t.Stops) Stops.Add(ss);
+            Stops.Add(s);
         }
     }
 
+}
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        IEnumerable<TSource> AllMins<TSource, TKey> (this IEnumerable<TSource> Source, Func<TSource, TKey> Comparer)
+        {
+            if (!typeof(IComparable).IsAssignableFrom(TKey)) throw new ArgumentException(nameof(TKey) + " must implement IComparable");
+
+            List<TSource> Values = new List<TSource>() { Source[0]};
+            TKey CurrentMin = Comparer(Source[0]);
+            for(int i = 1; i < Source.Count(); i++)
+            {
+                TKey CurrentValue = Comparer(Source[i]);
+                if (CurrentValue > CurrentMin) continue;
+                if (CurrentValue == CurrentMin) { Values.Add(Source[i]); continue; }
+                Values = new List<TSource>() { Source[i] };
+                CurrentMin = CurrentValue;
+            }
+            return Values;
+        }
+    }
 }
