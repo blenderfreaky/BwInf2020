a7e4dd3943090fcb8ce00ad853b12585ff400abc
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2019-11-09 01:42:12 +0100

    lato

--- Afg3Telepaartie/src/Telepaartie/State.cs
+++ Afg3Telepaartie/src/Telepaartie/State.cs
@@ -1,5 +1,6 @@
 using System.Security.AccessControl;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Extensions;
 
@@ -8,13 +9,36 @@ namespace Telepaartie
     public class State
     {
         public int iteration{get;}
-        public Tuple<int, int, int> Bucks {get;}
+        public List<int> Bucks {get;}
 
-        public State(State origin, Tuple<int, int> mover)
+        public State(int cupCount)
         {
-            iteration = origin.iteration+1;
-            Bucks
+            Bucks = new List<int>();
+            for (int i = 0; i < cupCount; i++)
+                Bucks.Add(0);
         }
 
+        private State(State origin, Tuple<int, int> mover)
+        {
+            iteration = origin.iteration+1;
+            Bucks = origin.Bucks.ToList();
+            ApplyOperation(mover);
+        }
+
+        public List<State> GetNextGen()
+        {
+            List<State> NextGen = new List<State>();
+            for(int i = 0; i < Bucks.Count; i++)
+                for(int u = 0; u < Bucks.Count; u++)
+                    if (u != i && i <= u) NextGen.Add(new State(this, new Tuple<int, int>(i, u)));
+            return NextGen;
+        }
+
+        private void ApplyOperation(Tuple<int, int> mover)
+        {
+            int val = Bucks[mover.Item1];
+            Bucks[mover.Item1] *= 2;
+            Bucks[mover.Item2] -= val;
+        }
     }
 }
