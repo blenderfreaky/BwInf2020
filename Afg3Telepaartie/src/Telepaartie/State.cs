1c325b877ab06c25f16909d372713dd1faf40d3d
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2019-11-09 12:25:47 +0100

    new Filos

--- Afg3Telepaartie/src/Telepaartie/State.cs
+++ Afg3Telepaartie/src/Telepaartie/State.cs
@@ -8,7 +8,10 @@ namespace Telepaartie
 {
     public class State
     {
-        public int iteration{get;}
+        public int Iterations { get => ((Daddy == null) ? (0) : (Daddy.Iterations + 1)); }
+
+        public State Daddy { get; private set; }
+        public List<State> Childos { get; private set; } = null;
         public List<int> Bucks {get;}
 
         public State(int cupCount)
@@ -16,22 +19,26 @@ namespace Telepaartie
             Bucks = new List<int>();
             for (int i = 0; i < cupCount; i++)
                 Bucks.Add(0);
+
+            Daddy = null;
         }
 
         private State(State origin, Tuple<int, int> mover)
         {
-            iteration = origin.iteration+1;
             Bucks = origin.Bucks.ToList();
             ApplyOperation(mover);
+
+            Daddy = origin;
         }
 
         public List<State> GetNextGen()
         {
-            List<State> NextGen = new List<State>();
+            if (Childos != null) throw new Exception("I am a father, I don't want more kids");
+            Childos = new List<State>();
             for(int i = 0; i < Bucks.Count; i++)
                 for(int u = 0; u < Bucks.Count; u++)
-                    if (u != i && i <= u) NextGen.Add(new State(this, new Tuple<int, int>(i, u)));
-            return NextGen;
+                    if (u != i && i <= u) Childos.Add(new State(this, new Tuple<int, int>(i, u)));
+            return Childos;
         }
 
         private void ApplyOperation(Tuple<int, int> mover)
@@ -39,6 +46,13 @@ namespace Telepaartie
             int val = Bucks[mover.Item1];
             Bucks[mover.Item1] *= 2;
             Bucks[mover.Item2] -= val;
+        }
+
+        private void HelpMeIDontWannaGetAdopted(State NewDaddy)
+        {
+            if (NewDaddy.Iterations >= Daddy.Iterations) throw new Exception("My daddy is far better");
+            Daddy.Childos.Remove(this);
+            Daddy = NewDaddy;
         }
     }
 }
