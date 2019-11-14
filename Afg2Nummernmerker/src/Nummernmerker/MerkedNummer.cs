c4716573e7f40ef3e50b5abcff37e4b067d45c26
blenderfreaky <blendcrafterxiv@gmail.com>
2019-11-14 11:28:43 +0100

    Doc

--- Afg2Nummernmerker/src/Nummernmerker/MerkedNummer.cs
+++ Afg2Nummernmerker/src/Nummernmerker/MerkedNummer.cs
@@ -4,12 +4,32 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    internal struct MerkedNummer : IEquatable<MerkedNummer>
+    /// <summary>
+    /// Represents the inputs to the Nummermerking problem, saving bools representing whether a digit is zero.
+    /// </summary>
+    internal readonly struct MerkedNummer : IEquatable<MerkedNummer>
     {
-        public bool[] Zeros;
-        public int MinSequenceLength;
-        public int MaxSequenceLength;
+        /// <summary>
+        /// The array indicating whether the n-th digit is zero. <langword>true</langword> for zero, <langword>false</langword> for not zero.
+        /// </summary>
+        public readonly bool[] Zeros;
 
+        /// <summary>
+        /// The lowest amount of digits that need to be in a segment of a distribution.
+        /// </summary>
+        public readonly int MinSequenceLength;
+
+        /// <summary>
+        /// The highest amount of digits that may be in a segment of a distribution.
+        /// </summary>
+        public readonly int MaxSequenceLength;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MerkedNummer"/> structure with the given values.
+        /// </summary>
+        /// <param name="zeros">The array indicating whether the n-th digit is zero. <langword>true</langword> for zero, <langword>false</langword> for not zero.</param>
+        /// <param name="minSequenceLength">The lowest amount of digits that need to be in a segment of a distribution.</param>
+        /// <param name="maxSequenceLength">The highest amount of digits that may be in a segment of a distribution.</param>
         public MerkedNummer(bool[] zeros, int minSequenceLength, int maxSequenceLength)
         {
             Zeros = zeros;
@@ -17,13 +37,17 @@
             MaxSequenceLength = maxSequenceLength;
         }
 
-        public override bool Equals(object obj) => obj is MerkedNummer other && Equals(other);
-        public bool Equals(MerkedNummer other) =>
+        /// <inheritdoc/>
+        public readonly override bool Equals(object obj) => obj is MerkedNummer other && Equals(other);
+
+        /// <inheritdoc/>
+        public readonly bool Equals(MerkedNummer other) =>
             Zeros.SequenceEqual(other.Zeros)
             && MinSequenceLength == other.MinSequenceLength
             && MaxSequenceLength == other.MaxSequenceLength;
 
-        public override int GetHashCode()
+        /// <inheritdoc/>
+        public readonly override int GetHashCode()
         {
             var hashCode = 1685608418;
             hashCode = (hashCode * -1521134295) + EqualityComparer<bool[]>.Default.GetHashCode(Zeros);
@@ -32,14 +56,10 @@
             return hashCode;
         }
 
-        public static bool operator ==(MerkedNummer left, MerkedNummer right)
-        {
-            return left.Equals(right);
-        }
+        /// <inheritdoc/>
+        public static bool operator ==(MerkedNummer left, MerkedNummer right) => left.Equals(right);
 
-        public static bool operator !=(MerkedNummer left, MerkedNummer right)
-        {
-            return !(left == right);
-        }
+        /// <inheritdoc/>
+        public static bool operator !=(MerkedNummer left, MerkedNummer right) => !(left == right);
     }
 }
--- Afg2Nummernmerker/src/Nummernmerker/NummerMerkingSummary.cs
+++ Afg2Nummernmerker/src/Nummernmerker/NummerMerkingSummary.cs
@@ -4,42 +4,66 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    public struct NummerMerkingSummary : IEquatable<NummerMerkingSummary>
+    /// <summary>
+    /// Represents a solution to a Nummermerking problem.
+    /// </summary>
+    public readonly struct NummerMerkingSummary : IEquatable<NummerMerkingSummary>
     {
-        public int[] Distribution;
-        public int LeadingZerosHit;
+        /// <summary>
+        /// The final calculated distribution.
+        /// </summary>
+        /// <remarks>
+        /// Lists the lengths of the segments in the distribution.
+        /// <code>
+        /// Distribution = new int[] { 2, 4, 3 };
+        /// 
+        /// ApplyDistribution("123456789")
+        /// > 12 3456 789
+        /// </code>
+        /// </remarks>
+        public readonly int[] Distribution;
 
+        /// <summary>
+        /// The amount of leading zeros that the distribution hits.
+        /// </summary>
+        /// <remarks>
+        /// <code>
+        /// Distribution = new int[] { 2, 4, 3 };
+        /// 
+        /// ApplyDistribution("12023103")
+        /// > 12 0231 03
+        /// 
+        /// LeadingZerosHit
+        /// > 2
+        /// </code>
+        /// </remarks>
+        public readonly int LeadingZerosHit;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NummerMerkingSummary"/> structure with the given values.
+        /// </summary>
+        /// <param name="distribution">The final calculated distribution.</param>
+        /// <param name="leadingZerosHit">The amount of leading zeros that the distribution hits.</param>
         public NummerMerkingSummary(int[] distribution, int leadingZerosHit)
         {
             Distribution = distribution;
             LeadingZerosHit = leadingZerosHit;
         }
 
-        public override bool Equals(object obj) => obj is NummerMerkingSummary other && Equals(other);
-
-        public bool Equals(NummerMerkingSummary other) =>
-            Distribution.SequenceEqual(other.Distribution)
-            && LeadingZerosHit == other.LeadingZerosHit;
-
-        public override int GetHashCode()
-        {
-            var hashCode = -1828418316;
-            hashCode = (hashCode * -1521134295) + EqualityComparer<int[]>.Default.GetHashCode(Distribution);
-            hashCode = (hashCode * -1521134295) + LeadingZerosHit.GetHashCode();
-            return hashCode;
-        }
-
-        public static bool operator ==(NummerMerkingSummary left, NummerMerkingSummary right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(NummerMerkingSummary left, NummerMerkingSummary right)
-        {
-            return !(left == right);
-        }
-
-        public IEnumerable<string> Apply(string text)
+        /// <summary>
+        /// Applies the distribution to the given text.
+        /// </summary>
+        /// <param name="text">The text to split up.</param>
+        /// <returns>An <see cref="IEnumerable{string}"/> yielding the segments of the distribution sequentially.</returns>
+        /// <remarks>
+        /// <code>
+        /// Distribution = new int[] { 2, 4, 3 };
+        /// 
+        /// ApplyDistribution("123456789").ToArray();
+        /// > string[] { "12", "3456", "789" }
+        /// </code>
+        /// </remarks>
+        public readonly IEnumerable<string> ApplyDistribution(string text)
         {
             int position = 0;
 
@@ -49,5 +73,30 @@
                 position += Distribution[i];
             }
         }
+
+        #region Overrides and Interface Implementations
+        /// <inheritdoc/>
+        public readonly override bool Equals(object obj) => obj is NummerMerkingSummary other && Equals(other);
+
+        /// <inheritdoc/>
+        public readonly bool Equals(NummerMerkingSummary other) =>
+            Distribution.SequenceEqual(other.Distribution)
+            && LeadingZerosHit == other.LeadingZerosHit;
+
+        /// <inheritdoc/>
+        public readonly override int GetHashCode()
+        {
+            var hashCode = -1828418316;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<int[]>.Default.GetHashCode(Distribution);
+            hashCode = (hashCode * -1521134295) + LeadingZerosHit.GetHashCode();
+            return hashCode;
+        }
+
+        /// <inheritdoc/>
+        public static bool operator ==(NummerMerkingSummary left, NummerMerkingSummary right) => left.Equals(right);
+
+        /// <inheritdoc/>
+        public static bool operator !=(NummerMerkingSummary left, NummerMerkingSummary right) => !(left == right);
+        #endregion
     }
 }
