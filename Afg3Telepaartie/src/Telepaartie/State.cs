cd823bb06b662220fbb6bc443c2584f63b9bc072
blenderfreaky <blendcrafterxiv@gmail.com>
2019-11-14 22:42:47 +0100

    bean time

--- Afg3Telepaartie/src/Telepaartie/State.cs
+++ Afg3Telepaartie/src/Telepaartie/State.cs
@@ -6,41 +6,31 @@ namespace Telepaartie
 
     public class State : IEquatable<State>
     {
-        #region Props & Fields
         public int Iterations { get => Daddy == null ? 0 : (Daddy.Iterations + 1); }
         public State Daddy { get; }
         public IReadOnlyList<int> Buckets { get; }
-        private int _hashCode;
-        #endregion
+        private readonly int _hashCode;
 
-        #region Constructor
-        public State(List<int> end)
+        public State(List<int> end, State daddy = null)
         {
             List<int> temp = end.ToList();
             temp.Sort();
             Buckets = temp;
-            Daddy = null;
-            UpdateHashCode();
+            Daddy = daddy;
+            _hashCode = CalculateHashCode();
         }
 
-        private State(State origin, (int, int) mover)
+        private static State ReverseTeelepartie(State daddy, int first, int second)
         {
-            void ApplyOperation(int first, int second, List<int> temp)
-        {
+            List<int> temp = daddy.Buckets.ToList();
+
             temp[first] /= 2;
             temp[second] += temp[first];
             temp.Sort();
+
+            return new State(temp, daddy);
         }
 
-            List<int> temp = origin.Buckets.ToList();
-            ApplyOperation(mover.Item1, mover.Item2, temp);
-            Buckets = temp;
-            Daddy = origin;
-            UpdateHashCode();
-        }
-        #endregion
-        
-        #region Funcs & Meth
         public IEnumerable<State> GetNextGen()
         {
             for (int i = 0; i < Buckets.Count; i++)
@@ -49,17 +39,18 @@ namespace Telepaartie
                 {
                     if (Buckets[i] % 2 == 0 && Buckets[i] > 0 && i != u)
                     {
-                        yield return new State(this, (i, u));
+                        yield return ReverseTeelepartie(this, i, u);
                     }
                 }
             }
         }
 
-        private void UpdateHashCode() =>
-            _hashCode = Buckets.Aggregate(168560841, (x, y) => (x * -1521134295) + y);
-        #endregion
-        
-        #region OverRides
+        private int CalculateHashCode() =>
+            Buckets.Aggregate(168560841, (x, y) => (x * -1521134295) + y);
+
+        #region Overrides and Interface Implementations
+        public override bool Equals(object obj) => obj is State state && Equals(state);
+
         public bool Equals(State state)
         {
             if (state == null) return false;
@@ -71,8 +62,6 @@ namespace Telepaartie
             }
             return true;
         }
-
-        public override bool Equals(object obj) => obj is State state && Equals(state);
 
         public override int GetHashCode() => _hashCode;
 
