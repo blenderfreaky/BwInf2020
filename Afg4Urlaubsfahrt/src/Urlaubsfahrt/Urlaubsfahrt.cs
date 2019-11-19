ab41f206dd7e973a15f6f5d9ad7c0e4fe2caa33f
doge815 <815.doge@gmail.com>
2019-11-19 12:46:33 +0100

    krinsch

--- Afg4Urlaubsfahrt/src/Urlaubsfahrt/Urlaubsfahrt.cs
+++ Afg4Urlaubsfahrt/src/Urlaubsfahrt/Urlaubsfahrt.cs
@@ -24,20 +24,13 @@
 
             foreach (GasStation station in allStations)
             {
-                trackParts.RemoveAll(x => station.Position - x.Stops.Last().Position < fuelLength &&
-                    x != Track.Empty);
+                trackParts.RemoveAll(x => station.Position - x.Stops.Last()
+                .Position < fuelLength &&x != Track.Empty);
 
                 List<Track> possibleParts = trackParts.ToList();
 
-                if (possibleParts.Count == 0) throw new InvalidOperationException("No solutions found");
-                else
+                if (!possibleParts.Count == 0)
                 {
-#if false
-                    Track t = new Track(possibleParts
-                        .AllMins(x => x.Stops.Count)
-                        .MinBy(x => x.GetPriceTo(station)), station);
-                    trackParts.Add(t);
-#else
                     List<Tuple<Track, float?>> bestTrackTuples = new List<Tuple<Track, float?>>();
                     possibleParts
                         .AllMins(x => x.Stops.Count)
@@ -47,8 +40,8 @@
                             .Add(new Tuple<Track, float?>(x, x.GetPriceTo(station))));
                     bestTrackTuples.RemoveAll(x => x.Item2 == null);
                     bestTrackTuples.Select(x => x.Item1).ToList().ForEach(x => possibleParts.Add(x.Append(station)));
-#endif
                 }
+                else throw new InvalidOperationException("No solutions found");
             }
             return trackParts.Last();
         }
