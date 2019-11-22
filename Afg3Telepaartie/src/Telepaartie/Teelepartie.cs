764a030ab069e713fb9997d65dd448f1d86e315d
doge815 <815.doge@gmail.com>
2019-11-22 15:46:52 +0100

    doc u other stuff how long can a commit name b

--- Afg3Telepaartie/src/Telepaartie/Teelepartie.cs
+++ Afg3Telepaartie/src/Telepaartie/Teelepartie.cs
@@ -36,8 +36,8 @@
             State? goal,
             Action<string>? writeLine)
         {
-            List<State> newDads = GetEndingStates(numberOfCups, numberOfItems)  //Alle Endzustände bilden die nullte Generation
-.Select(x => new State(x))
+            List<State> lastGen = GetEndingStates(numberOfCups, numberOfItems)  //Alle Endzustände bilden die nullte Generation
+                .Select(x => new State(x))
                 .ToList();
 
             List<State> allStates = lastGen
@@ -51,14 +51,14 @@
                     .AsParallel()
                     .SelectMany(x => x.GetNextGen())    //Erschaffe aus jedem Element die Kinder
                     .Distinct()                         //Töte die doppelten Kinder
-                    .Except(allOlds.AsParallel())       //Töte die Kinder, die schon in den Alten vorhanden sind
+                    .Except(allStates.AsParallel())       //Töte die Kinder, die schon in den Alten vorhanden sind
                     .ToList();
 
                 if (goal != null)                                   //Falls die Operationsanzahl für nur 1 Zustand festgestellt werden soll
                 {
-                    if (newChildos.Contains(goal)) return i + 1;    //Wenn das Element in den neuen Kindern vorhanden ist, gebe die Operationsanzahl zurück zurück
+                    if (nextGen.Contains(goal)) return i + 1;    //Wenn das Element in den neuen Kindern vorhanden ist, gebe die Operationsanzahl zurück zurück
                 }
-                else if (newChildos.Count == 0)                 //Wenn keine neuen Kinder gefunden worden sind
+                else if (nextGen.Count == 0)                 //Wenn keine neuen Kinder gefunden worden sind
                 {
                     writeLine?.Invoke(Environment.NewLine);
                     foreach (var oldestChild in lastGen)
@@ -81,32 +81,36 @@
             }
         }
 
-        private static List<List<int>> GetEndingStates(int NumberOfCups, int NumberOfItems)
+        private static IEnumerable<List<int>> GetEndingStates(int numberOfCups, int numberOfItems)
         {
-            List<List<int>> states = GetStates(NumberOfCups - 1, NumberOfItems);
-
-            foreach (var state in states) state.Insert(0, 0);
-
-            return states;
+            return GetStates(numberOfCups, numberOfItems).Do(s => s.Add(0));
         }
 
-        private static List<List<int>> GetStates(int numberOfCups, int numberOfItems, int max = -1)
+private static IEnumerable<T> Do<T>(this IEnumerable<T> enumerable, Action<T> action)
+{
+                    foreach (var s in enumerable)
+                    {
+                        action(s);
+                        yield return s;
+                    }
+}
+
+private static IEnumerable<T> Yield<T>(this T t) { yield return t; }
+
+        private static IEnumerable<List<int>> GetStates(int numberOfCups, int numberOfItems, int max = -1)
         {
             if (max == -1) max = numberOfItems;
             if (numberOfCups < 1) throw new ArgumentException();
-            if (numberOfCups == 1) return new List<List<int>> { new List<int> { numberOfItems } };
+            if (numberOfCups == 1) return (new List<int> { numberOfItems }).Yield();
 
             int min = (int)Math.Ceiling(numberOfItems / (decimal)numberOfCups);
-            return Enumerable.Range(min, Math.Min(max - min + 1, numberOfItems - min))
-                .SelectMany(i =>
-                {
-                    List<List<int>> states = GetStates(numberOfCups - 1, numberOfItems - i, i);
-                    foreach (var state in states) state.Add(i);
-                    return states;
-                })
-                .ToList();
 
-            throw new ArgumentException();
+            return Enumerable.Range(min, Math.Min(max - min + 1, numberOfItems - min))
+                .AsParallel()
+                .SelectMany(i => 
+                    GetStates(numberOfCups - 1, numberOfItems - i, i)
+                    .Do(s => s.Add(i)));
         }
+
     }
 }
